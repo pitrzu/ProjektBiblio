7d0e64e70f83fd66e4318fe855e09f55104f17c2
Patryk Manka <63506786+M4niek23@users.noreply.github.com>
2024-11-24 00:00:04 +0100

    Gui & Authorization beta version
    I add Gui and Authorization I'm going to build it up in the next version it will be a functional version of the this.

--- WSB.Biblioteka/Users/User.cs
+++ WSB.Biblioteka/Users/User.cs
@@ -1,9 +1,55 @@
-﻿namespace WSB.Biblioteka.Users;
-
-public class User
+﻿namespace WSB.Biblioteka.Users
 {
-    public Guid Id { get; private set; }
-    public string Name { get; private set; } 
-    public string Login { get; private set; }   
-    public string Password { get; private set; } 
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Login { get; private set; }
+        public string PasswordHash { get; private set; }
+
+        private User(Guid id, string name, string login, string passwordHash)
+        {
+            Id = id;
+            Name = name;
+            Login = login;
+            PasswordHash = passwordHash;
+        }
+
+        public static User Register(string name, string login, string password)
+        {
+            ValidateInput(name, nameof(name));
+            ValidateInput(login, nameof(login));
+            ValidatePassword(password);
+
+            var passwordHash = HashPassword(password);
+            return new User(Guid.NewGuid(), name, login, passwordHash);
+        }
+
+        private static void ValidateInput(string input, string paramName)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException(paramName, $"{paramName} nie może być puste.");
+        }
+
+        private static void ValidatePassword(string password)
+        {
+            if (password.Length < 6)
+                throw new ArgumentException("Hasło musi mieć co najmniej 6 znaków.", nameof(password));
+        }
+
+        private static string HashPassword(string password)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA256())
+            {
+                var salt = Guid.NewGuid().ToByteArray();
+                var hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password + Convert.ToBase64String(salt)));
+                return Convert.ToBase64String(hash) + ":" + Convert.ToBase64String(salt);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"User: {Name} (Login: {Login}, Id: {Id})";
+        }
+    }
 }
